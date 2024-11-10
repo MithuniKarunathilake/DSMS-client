90863e8bd2258b8b96df48b2cd33faf75de9c427
mithu0621 <mithunisk03@gmail.com>
2024-11-10 22:01:49 +0530

    manage instructor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './employee/login/login.component';
 import { RegisterComponent } from './employee/register/register.component';
 import { HomeComponent } from './common/home/home.component';
+import { ManageStudentComponent } from './pages/manage-student/manage-student.component';
+import { ManageInstructorComponent } from './pages/manage-instructor/manage-instructor.component';
+import { ManagePackagesComponent } from './pages/manage-packages/manage-packages.component';
+import { ShedularComponent } from './pages/shedular/shedular.component';
 
 export const routes: Routes = [
     {
@@ -17,5 +21,25 @@ export const routes: Routes = [
 
         path: 'register',
         component: RegisterComponent,
+    },
+    {
+
+        path: 'manage-student',
+        component: ManageStudentComponent,
+    },
+    {
+
+        path: 'manage-instructor',
+        component: ManageInstructorComponent,
+    },
+    {
+
+        path: 'manage-package',
+        component: ManagePackagesComponent,
+    },
+    {
+
+        path: 'shedular',
+        component: ShedularComponent,
     }
 ];
--- src/app/pages/manage-instructor/manage-instructor.component.ts
+++ src/app/pages/manage-instructor/manage-instructor.component.ts
@@ -1,12 +1,67 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../../common/header/header.component';
 
 @Component({
   selector: 'app-manage-instructor',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, HttpClientModule,HeaderComponent],
   templateUrl: './manage-instructor.component.html',
   styleUrl: './manage-instructor.component.css'
 })
 export class ManageInstructorComponent {
+  public user: any = {
+    name: "",
+    address: "",
+    email: "",
+    password: "",
+    dob: "",
+    contactNumber: "",
+    experiencedYears:""
+  };
+
+  constructor(private http: HttpClient) {
+    this.loadTable();
+  }
+
+  public addUser() {
+    this.http.post("http://localhost:8080/instructor/add-instructor", this.user).subscribe((data) => {
+      alert("User Added!!!!");
+    })
+  }
+
+
+  public userList: any = [];
+
+  loadTable() {
+    this.http.get("http://localhost:8080/instructor/get-instructor").subscribe(data => {
+      console.log(data);
+      this.userList = data;
+    })
+  }
+
+  deleteUserById(id: any) {
+    console.log(id);
+
+    this.http.delete(`http://localhost:8080/instructor/delete-instructor/${id}`).subscribe(data => {
+      alert("User deleted !!!!");
+      this.loadTable();
+    })
+
+  }
+  public userTemp: any = {}
+  updateUser(user: any) {
+    console.log(user);
+    this.userTemp = user;
+
+  }
+
+  saveUser() {
+    this.http.put("http://localhost:8080/instructor/update-instructor", this.userTemp).subscribe(data => {
+      alert("User Updated!!!!!")
+    })
+  }
 
 }
